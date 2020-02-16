82c4ab7cc00861631e62649a39555b0aa1101337
ibrahimcss <ibrahimcss@outlook.com>
2020-02-17 00:43:36 +0300

    Initial commit

---
+++ src/main/java/com/example/post/demo/repository/CommentRepository.java
@@ -0,0 +1,15 @@
+package com.example.post.demo.repository;
+
+import com.example.post.demo.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findByPostId(Long postId, Pageable pageable);
+    Optional<Comment> findByIdAndPostId(Long id, Long postId);
+}
---
+++ src/main/java/com/example/post/demo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.example.post.demo.repository;
+
+
+import com.example.post.demo.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
